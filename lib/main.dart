6f454e1c38c38940f70565ba7c56ffaff7485d85
masaki.takayuki <create.to.music@gmail.com>
2026-04-04 00:01:17 +0900

    ホームタブで何も表示されない問題解消

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutter_timezone/flutter_timezone.dart';
+import 'package:intl/date_symbol_data_local.dart';
 import 'package:timezone/data/latest.dart' as tz;
 import 'package:timezone/timezone.dart' as tz;
 
@@ -13,6 +14,7 @@ import 'services/notification_service.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  await initializeDateFormatting('ja');
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
